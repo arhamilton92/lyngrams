23af5e0aac3f482805022466d3c568c3883002f7
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-09-19 19:07:00 -0400

    initial commit. ran gh-pages build

---
+++ src/grams.js
@@ -0,0 +1,20 @@
+
+// date format is strict. 2010-10-20 4:30, "YYYY-MM-DD HH:mm"
+// do not remove any commas, paranthesis or apostrophes inside the below object.
+
+const grams = {
+    bananamantis: {
+        headerURl: 'https://sta.sh/0lcea8yfrjo',
+        FooterURL: 'https://sta.sh/01p4ehaupk2a',
+        unopenedImageURL: 'https://cdn.discordapp.com/attachments/749273955822731355/756993916955263048/EasterUnopenedB.png',
+        pageBackgroundURL: 'https://pbs.twimg.com/media/D4kFDw5W0AAoAXg.png',
+        messageBoxURL: '',
+        messagePlaceholder: '',
+        messageReveal: 'HI I made you a super cute gram.',
+        gramUrl: 'https://pbs.twimg.com/media/D4kRPk_WkAEyAPj.png',
+        openDate: '',
+        stashLink: 'https://sta.sh/2212buansezz',
+    }
+}
+
+export default grams;
